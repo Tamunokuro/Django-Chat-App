15787a0dbd31f01858ad9184538aa92308b9d26c
Tamunokuro <tamunokrobluejack@gmail.com>
2023-06-24 16:22:33 +0100

    Restructure directory

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,24 +3,18 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 class ChatConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
+        super().__init__(*args, **kwargs)
         self.room_name = None
 
     def connect(self):
-        print("Connected!")
-        self.room_name = "home"
+        print("Connected")
+        self.room_name = "Party"
         self.accept()
-        self.send_json(
-            {
-                "type": "welcome_message",
-                "message": "Hey there! You've successfully connected!",
-            }
-        )
+        self.send_json({"type": "chat", "text": "Welcome to the chat room!"})
 
     def disconnect(self, code):
-        print("Disconnected!")
+        print("Disconnected")
         return super().disconnect(code)
-
+    
     def receive_json(self, content, **kwargs):
-        print(content)
         return super().receive_json(content, **kwargs)
---
+++ config/routing.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from django_chat.chat.consumers import ChatConsumer
+
+websocket_urlpatterns = [path("", ChatConsumer.as_asgi())]
