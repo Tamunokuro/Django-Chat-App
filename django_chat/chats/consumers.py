5839eb63c4b5f8dd5dec841c355a0ed6c61261f0
Tamunokuro <tamunokrobluejack@gmail.com>
2023-07-03 14:11:44 +0100

    Polish basic chat setup 🥳

--- django_chat/chats/consumers.py
+++ django_chat/chats/consumers.py
@@ -1,3 +1,4 @@
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 
 
@@ -10,6 +11,13 @@ class ChatConsumer(JsonWebsocketConsumer):
         print("Connected")
         self.room_name = "Party"
         self.accept()
+
+        #accept connection
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name
+        )
+        #send json message
         self.send_json({"type": "chat", "text": "Welcome to the chat room!"})
 
     def disconnect(self, code):
@@ -18,6 +26,18 @@ class ChatConsumer(JsonWebsocketConsumer):
     
     def receive_json(self, content, **kwargs):
         message_type = content["type"]
-        if message_type == "message":
-            print(content["message"])
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                },
+            )
+           
         return super().receive_json(content, **kwargs)
+
+    def chat_message_echo(self, event):
+        print(event)
+        self.send_json(event)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,13 @@ import useWebSocket, { ReadyState } from "react-use-websocket";
 
 
 export default function App() {
-  const [welcomeMessage, setWelcomeMessage] = useState("")
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+
+  const [messageHistory, setMessageHistory] = useState([])
+
   const { readyState, sendJsonMessage } = useWebSocket("ws://127.0.0.1:8000/", {
     onOpen: () => {
       console.log('Connection Successful')
@@ -17,9 +23,13 @@ export default function App() {
         case "chat":
           setWelcomeMessage(data.text);
           break;
-        default:
-          e.error("Unknown message")
+        case "chat_message_echo":
+          setMessageHistory((prev) => prev.concat(data));
           break;
+        default:
+          console.error("Unknown message")
+          break;
+        
       }
   }
   })
@@ -32,20 +42,44 @@ export default function App() {
     [ReadyState.UNINSTANTIATED]: "Uninitiated"
   }[readyState];
 
+  function handleChangeMessage(e) {
+    setMessage(e.target.value);
+  }
+  
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(){
+    sendJsonMessage({
+      type: "chat_message",
+      message,
+      name
+    });
+    setName("");
+    setMessage("");
+  }
+
 
   return (
-    <div>
+    <div className="flex flex-col justify-content-center">
       <span>The websocket is currently {connectionStatus}</span>
       <p>{welcomeMessage}</p>
-     <button className="bg-gray-300 px-3 py-1"
-     onClick={() => {
-      sendJsonMessage({
-        type:"message",
-        message: "Hey there!"
-      })
-     }}>
-      Send Message!
+
+     <input className="w-80" name="name" placeholder="Name" onChange={handleChangeName} value={name} />
+     <input className="w-80" name="message" placeholder="Message" onChange={handleChangeMessage} value={message} />
+     <button className="ml-3 bg-gray-300 px-3 py-3 my-3 w-80" onClick={handleSubmit}>
+      Send
      </button>
+
+     <hr />
+     <ul>
+     {messageHistory.map((message, idx) => (
+      <div className='border border-gray-200 py-3 px-3' key={idx}>
+        {message.name}: {message.message}
+      </div>
+     ))}
+     </ul>
     </div>
   )
 }
