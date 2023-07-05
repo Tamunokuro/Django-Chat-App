7627420e9b339349c08b535ee7e56f735b923287
Tamunokuro <tamunokrobluejack@gmail.com>
2023-07-05 14:27:33 +0100

    Connect login from frontend to backend 🔌

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Chat } from "./components/Chat";
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar";
+
+// imports
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthContextProvider>
+              <Navbar />
+            </AuthContextProvider>
+          }
+        >
+          <Route path="" element={<Chat />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
