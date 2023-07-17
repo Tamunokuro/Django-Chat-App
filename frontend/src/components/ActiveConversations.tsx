b07180f79027af7df04c8bb75ce8d450ee053466
Tamunokuro <tamunokrobluejack@gmail.com>
2023-07-17 18:59:28 +0100

    Document code and fix linter errors

--- frontend/src/components/ActiveConversations.tsx
+++ frontend/src/components/ActiveConversations.tsx
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import { ConversationModel } from '../models/Conversation';
 
+/**
+ * The `ActiveConversations` component fetches and displays a list of active conversations, including
+ * the usernames of the other users involved and the last message sent in each conversation.
+ * @returns The ActiveConversations component returns a JSX element that contains a list of
+ * conversation items. Each conversation item is wrapped in a Link component and contains the username
+ * of the other user in the conversation, the last message in the conversation, and the timestamp of
+ * the last message.
+ */
 export function ActiveConversations() {
   const { user } = useContext(AuthContext);
   const [conversation, setActiveConversation] = useState<ConversationModel[]>(
@@ -22,11 +30,25 @@ export function ActiveConversations() {
     fetchUsers();
   }, [user]);
 
+  /**
+   * The function creates a conversation name by sorting and concatenating two usernames.
+   * @param {string} username - The `username` parameter is a string that represents the username of a
+   * user.
+   * @returns a string that is created by concatenating the two usernames in alphabetical order,
+   * separated by two underscores.
+   */
   function createConversationName(username: string) {
     const nameAlph = [user?.username, username].sort();
     return `${nameAlph[0]}__${nameAlph[1]}`;
   }
 
+  /**
+   * The function `formatMessageTimestamp` formats a given timestamp into a localized time string.
+   * @param {string} [timestamp] - The `timestamp` parameter is a string representing a date and time.
+   * It is an optional parameter, meaning it can be omitted when calling the `formatMessageTimestamp`
+   * function.
+   * @returns a formatted timestamp string.
+   */
   function formatMessageTimestamp(timestamp?: string) {
     if (!timestamp) return;
     const date = new Date(timestamp);
